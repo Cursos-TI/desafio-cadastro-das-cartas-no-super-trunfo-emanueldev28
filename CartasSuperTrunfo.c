8a7b4ac1ef0d0e2c0d91133bdcc96d0a2af5cf09
emanueldev28 <emanuelpm1353@gmail.com>
2025-03-20 22:27:34 +0000

    finaliei as cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 //desafio das cartas
+//dados carta 1
 
 int main() {
 
@@ -31,6 +32,10 @@ scanf("%d", &POPULACAO1);
 printf("Digite os pontos turisticos:");
 scanf("%d", &PONTOS1);
 
+
+//impressoes carta 1
+
+
 printf("carta 1.\n");
 printf("ESTADO: %c\n" ,ESTADO1);
 printf("CIDADE: %s\n" ,CIDADE1);
@@ -40,6 +45,42 @@ printf("AREA: %.2fkm2\n" ,AREA1);
 printf("POPULACAO: %d\n" ,POPULACAO1);
 printf("PONTOS TURISTICOS: %d\n" ,PONTOS1);
 
+
+//carta 2
+
+//ESTADO 2
+printf("Digite o estado: ");
+scanf("%c", ESTADO2);
+//CIDADE 2
+printf("Digite a cidade: ");
+scanf("%s", CIDADE2);
+//CODIGO 2
+printf("Digite o codigo: ");
+scanf("%s" ,CODIGO2);
+//PIB 2
+printf("Digite o PIB: ");
+scanf("%f", &PIB2);
+//AREA 2
+printf("Digite a area: ");
+scanf("%f", &AREA2);
+//POPULACAO 2
+printf("Digite a populacao: ");
+scanf("%d", &POPULACAO2);
+//PONTOS TURISTICOS 2
+printf("Digite os pontos turisticos: ");
+scanf("%d"), &PONTOS2;
+
+//impressoes carta 2
+
+
+printf("Carta 2.");
+printf("Estado: %c\n", ESTADO2);
+printf("Cidade: %S\n", CIDADE2);
+printf("Codigo: %s\n", CODIGO2);
+printf("PIB: %.2f\n", &PIB2);
+printf("Area: %,2f\n", &AREA2);
+printf("Populacao: %d\n", &POPULACAO2);
+printf("Pontos Turisticos: %d\n", &PONTOS2);
 
 
 
